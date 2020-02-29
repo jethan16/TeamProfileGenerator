2cf447ee87961326c2b46a9196cadaf11e6c6f5c
Jethan16 <ethan.nel.jones@gmail.com>
2020-02-29 11:10:54 -0500

    added inquirer prompts and input dependant questions

--- Develop/app.js
+++ Develop/app.js
@@ -4,30 +4,169 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
+const answersArr = []
+
+
+function init() {
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "which type of team member would you like to add?",
+            choices: [
+                'Engineer',
+                'Intern'
+            ],
+            name: 'profileType',
+            default: 'Engineer'
+        }
+    ]).then(results => {
+
+        if (results.profileType === 'Engineer') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: `What is your engineer's name?`,
+                    name: `name`
+                },
+                {
+                    type: 'input',
+                    message: `What is your engineer's id`,
+                    name: 'id',
+                },
+                {
+                    type: 'input',
+                    message: `What is your engineer's email`,
+                    name: 'email'
+                },
+                {
+                    type: 'input',
+                    message: `What is your engineer's GitHub username?`,
+                    name: 'github'
+                },
+                {
+                    type: 'list',
+                    message: `Would you like to add another team member?`,
+                    choices: ['yes', 'no'],
+                    name: 'restart'
+                },
+            ]).then(results => {
+
+                makeList(results)
+
+            })
+        } else {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: `What is your intern's name?`,
+                    name: 'name',
+                },
+                {
+                    type: 'input',
+                    message: `What is your intern's id?`,
+                    name: 'id',
+                },
+                {
+                    type: 'input',
+                    message: `What is your intern's email?`,
+                    name: 'email',
+                },
+                {
+                    type: 'input',
+                    message: `Where did/does your intern attend school?`,
+                    name: 'school ',
+                },
+                {
+                    type: 'list',
+                    message: `Would you like to add another team member?`,
+                    choices: ['yes', 'no'],
+                    name: 'restart'
+                },
+            ]).then(results => {
+
+                makeList(results)
+
+            })
+        }
+    });
+}
+
+function makeList(answers) {
+
+    // answersArr.push(answers)
+
+    if (answers.restart === 'yes') {
+        console.log('answers in app.js', answers);
+        init()
+    } else {
+        console.log('answers in app.js', answers);
+    }
+    
+    module.exports = answers
+
+    // render(answersArr)
+
+    // module.exports = answers;
+}
+
+init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Employee class
+const answers = require('../app')
+
+
+console.log("answers in Employee.js", answers);
+
+class Employee {
+    
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    
+    getName() {
+        this.name = answers.name
+    }
+
+    // getId() {
+
+    // }
+
+    // getEmail() {
+
+    // }
+
+    getRole() {
+        return 'Employee'
+    }
+}
+
+const bob = new Employee('bob', 001, 'bob@fakemail.com')
+
+module.exports = Employee;
